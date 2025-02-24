05910b69d54beea0420d0162f1ec88e62718d8ea
Dave Christenson <Dave.Christenson@ibm.com>
2025-02-24 08:34:44 -0600

    Improve JS Editor

--- plugins/parsejson/src/plugin.ts
+++ plugins/parsejson/src/plugin.ts
@@ -29,8 +29,8 @@ export function parseJSON(
 	let level = 'info';
 	let date = new Date();
 	let category = '';
-	let kind = 'Kind_is_not_set';
-	let message = 'Message is not set - edit or replace client/public/parsejson/plugin.js';
+	let kind = 'Kind_is_not_defined';
+	let message = `Message field not defined - click '?'`;
 	let additionalJSON: { [key: string]: any } = {};
 	const ignoreFields: string[] = [];
 
@@ -50,7 +50,93 @@ export function parseJSON(
 			additionalJSON.level = level;
 		}
 	} else { // Check for other log formats
+		let dateSet = false;
+		let levelSet = false;
+		let kindSet = false;
+		let messageSet = false;
 
+		const fieldValues: { field: string, value: string }[] = []; // array of {field, value} pairs  
+
+		// Recursively traverse JSON and build fieldValues array
+		function traverseJson(obj: { [key: string]: any }) {
+			for (let field in obj) {
+				const value = obj[field];
+				if (typeof field === 'string' && typeof value === 'string') {
+					field = field.toLowerCase();
+					fieldValues.push({ field, value });
+				} else if (typeof value === 'object' && !Array.isArray(value)) {
+					traverseJson(value);
+				}
+			}
+		}
+		traverseJson(jsonObject);
+
+		// Check each JSON field,value pair looking for date, info, kind and message
+		for (const fieldValue of fieldValues) {
+			const field = fieldValue.field;
+			const value = fieldValue.value;
+
+			checkForDateLevelKindMessage(field, value);
+
+			if (dateSet && levelSet && kindSet && messageSet) break;
+		}
+
+		// Check for data, level, kind and message fields
+		function checkForDateLevelKindMessage(field: string, value: string) {
+			if (!dateSet) {
+				if ((field.includes('time') || field.includes('date')) && isValidDate(value)) {
+					dateSet = true;
+					date = new Date(value);
+					return;
+				}
+			}
+
+			if (!levelSet) {
+				if (field === 'level' || field === 'severity') {
+					levelSet = true;
+					level = value;
+					return;
+				} else if (field === 'error') {
+					levelSet = true;
+					level = 'error';
+
+				}
+			}
+
+			if (!kindSet) {
+				if (field.length <= 64 && (field === 'kind' || field.startsWith("thread") || field.startsWith('app'))) {
+					kindSet = true;
+					kind = value;
+					return;
+				}
+			}
+
+			if (!messageSet) {
+				if (field === 'message' || field === 'error') {
+					messageSet = true;
+					message = value;
+					return;
+				}
+			}
+		}
+
+		// Returns true, if this is a valid date
+		function isValidDate(value: string) {
+			try {
+				let date = new Date(value);
+				if (date.toString() === 'Invalid Date' && typeof value === 'string') {
+					const tokens = value.split(':', 2);
+					if (tokens.length === 2) {
+						let d = new Date(tokens[0]);
+						date = new Date(d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ':' + tokens[1]);
+					}
+				}
+				if (date.toString() === 'Invalid Date') return false;
+			} catch (e) {
+				return false;
+			}
+			return true;
+		}
 	}
 
 	return { date, level, category, kind, message, rawLine: undefined, additionalJSON, ignoreFields };
